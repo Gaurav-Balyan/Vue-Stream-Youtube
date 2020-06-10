4d52e2f4c46e12fbcb60fcb60cfb6c01281691d0
Gaurav Balyan <gaurav.balyan24@gmail.com>
2020-06-10 13:48:26 +0530

    Created list

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <input @input="onInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.$emit('termChange', 'cars');
+  },
+
+  name: 'SearchBar',
+  methods: {
+    onInput: function(event) {
+      this.$emit('termChange', event.target.value);
+    },
+  },
+};
+</script>
+
+<style scoped>
+input {
+  width: 75%;
+}
+div {
+  text-align: center;
+  margin: 20px;
+}
+</style>
